792b92d33d177686ed898f658ecee237ad4bd2a3
Edson Junior <ejr_04@outlook.com>
2021-05-06 14:18:15 -0300

    Exercicios Concluidos

---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                float nota;
+                bool check = true;
+                string texto = "";
+                while (check == true)
+            {
+                Console.WriteLine("Insira sua nota" + texto);
+                nota = float.Parse(Console.ReadLine());
+
+                 if (nota >= 0 && nota <=10)
+                {
+                Console.WriteLine("Nota valida");
+                check = false;
+                }
+                else
+                {
+                    Console.WriteLine("Nota invalida");
+                    texto = " novamente";
+                }
+            }               
+        }
+      
+    }
+}
---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool check = true;
+            string texto = "";
+            string nome = "";
+            string senha = "";
+            while (check == true)
+        {
+              Console.WriteLine("Insira o seu nome" + texto);
+              nome = Console.ReadLine();
+              //Console.WriteLine(nome);
+              Console.WriteLine("Insira o seu senha" + texto);
+              senha = Console.ReadLine();
+              //Console.WriteLine(senha);
+            
+            if (nome == senha)
+              {
+                  Console.WriteLine("Cadastro inválido: Nome igual a senha");
+                texto = " novamente:";
+              }
+              else
+              
+              {
+                   Console.WriteLine("Cadastro Efetuado !!!");
+                    check = false;
+                      texto = "novamente";
+              }
+        }
+
+        }
+    }
+}
